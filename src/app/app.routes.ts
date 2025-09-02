c8f717f3ff32d7d592138b0e18437fa7658c63d1
Banuka Malshan <bmagamulla@gmail.com>
2025-09-02 16:04:26 +0530

    1.Create register component,for new users. 2.Create routes for ('/') Login component and ('/register') Register component. 3. Create template view for login component. 4. Add all required imports from @angular/material. 4. Create formcontrols for login template in login component ts file. 5. Add validators for each formcontroll

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,14 @@
 import { Routes } from '@angular/router';
+import { Login } from './login/login';
+import { Register } from './register/register';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {path:'',
+     title:"Login",
+     component:Login   
+    },
+    {path:'register',
+        title:"Register",
+        component:Register
+    },
+];
--- src/app/login/login.ts
+++ src/app/login/login.ts
@@ -1,11 +1,44 @@
-import { Component } from '@angular/core';
+import { Component, inject, signal } from '@angular/core';
+import { RouterLink } from '@angular/router';
+import { FormBuilder, FormControl, FormGroup, ReactiveFormsModule, Validators}from '@angular/forms';
+import {MatSelectModule} from '@angular/material/select';
+import {MatInputModule} from '@angular/material/input';
+import {MatFormFieldModule} from '@angular/material/form-field';
+import {MatIconModule} from '@angular/material/icon';
+import {MatCardModule} from '@angular/material/card';
+import {MatButtonModule} from '@angular/material/button';
 
 @Component({
   selector: 'app-login',
-  imports: [],
+  imports: [RouterLink,ReactiveFormsModule,MatButtonModule,MatCardModule,MatIconModule,MatFormFieldModule,MatInputModule,MatSelectModule],
   templateUrl: './login.html',
   styleUrl: './login.css'
 })
 export class Login {
+   errorMessage=signal("Email is required");
+  loginForm!:FormGroup;
+  #formBuilder=inject(FormBuilder);
+
+   ngOnInit(): void {
+    this.loginForm=this.#formBuilder.group({
+      email:new FormControl('',[
+        Validators.required,
+        Validators.email
+      ]),
+      password:new FormControl('',[
+        Validators.required,
+        Validators.minLength(6)
+      ])
+    });
+    
+  }
+
+
+  hide = signal(true);
+  clickEvent(event: MouseEvent) {
+    event.preventDefault();
+    this.hide.set(!this.hide());
+    
+  }
 
 }
