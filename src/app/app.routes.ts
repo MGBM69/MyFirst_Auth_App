d7b96b998e3ff970b8048052408dc3a5a9ce4995
Banuka Malshan <bmagamulla@gmail.com>
2025-09-04 13:39:26 +0530

    1. Create component to implement Forgot password feature to reset password. 2.  Create commponent to page-not-found to show invalid url and create route to that.

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,6 +2,7 @@ import { Routes } from '@angular/router';
 import { Login } from './login/login';
 import { Register } from './register/register';
 import { Home } from './home/home';
+import { PageNotFound } from './page-not-found/page-not-found';
 
 export const routes: Routes = [
     {path:'',
@@ -16,5 +17,10 @@ export const routes: Routes = [
         path:'home',
         title:"Home",
         component:Home
+    },
+    {
+        path:'**',
+        title:"Page Not Found",
+        component:PageNotFound
     }
 ];
